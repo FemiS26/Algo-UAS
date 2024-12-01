81f8dd42cf6acaa3644bab96083421a274de46e3
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2024-12-01 22:00:22 +0700

    Update King Jawa
    ISi Atribut

--- King.java
+++ King.java
@@ -1,3 +1,14 @@
 public class King {
-    
+    private String kingName, kingSkills;
+    private double kingHP, kingAttack, kingDefense;
+
+    public King(String name, String skills, double HP, double Attack, double Defense) {
+
+    }
+
+    // Setter and Getter
+    public void setName(String name) {
+        this.kingName = name;
+    }
+
 }
