44d7189e933c89578b4347e1fb0bed63d051ae70
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2024-12-04 22:25:14 +0700

    Pindah battle ke oop

--- Main Codes and Others/Main2.java
+++ Main Codes and Others/Main2.java
@@ -125,43 +125,4 @@ public class Main2{
         }
     }
 // End of Print All King
-
-
-    public static void battleSimulation() {
-        if (kingCount < 2) {
-            System.out.println("At least two kings are required for a battle.");
-            return;
-        }
-
-        System.out.print("Enter the index of the first king: ");
-        int index1 = scanner.nextInt();
-        System.out.print("Enter the index of the second king: ");
-        int index2 = scanner.nextInt();
-
-        if (index1 >= 0 && index1 < kingCount && index2 >= 0 && index2 < kingCount && index1 != index2) {
-            Stats king1 = statsArray[index1];
-            Stats king2 = statsArray[index2];
-
-            System.out.println("\nStarting battle between " + king1.getName() + " and " + king2.getName() + "!");
-            while (king1.getHp() > 0 && king2.getHp() > 0) {
-                king2.takeDamage(king1.getAttackPower());
-                System.out.println(king1.getName() + " attacks " + king2.getName() + "! " + king2.getName() + " HP: " + king2.getHp());
-
-                if (king2.getHp() <= 0) {
-                    System.out.println(king2.getName() + " has been defeated! " + king1.getName() + " wins!");
-                    break;
-                }
-
-                king1.takeDamage(king2.getAttackPower());
-                System.out.println(king2.getName() + " attacks " + king1.getName() + "! " + king1.getName() + " HP: " + king1.getHp());
-
-                if (king1.getHp() <= 0) {
-                    System.out.println(king1.getName() + " has been defeated! " + king2.getName() + " wins!");
-                    break;
-                }
-            }
-        } else {
-            System.out.println("Invalid indices for battle.");
-        }
-    }
 }
