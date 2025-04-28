311ae384ba3693a27ccff553d672982233719d6d
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2025-04-28 13:41:11 +0700

    salah

--- OOPFifth/VirtualOperation.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-interface VirtualOperations {
-    void deposit(double amount);
-    void transfer(String receiverAccount, double amount, ArrayList<VirtualAccount> accounts);
-}
